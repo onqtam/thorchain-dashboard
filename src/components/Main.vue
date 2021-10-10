0c705a4e60d6f77088833c44879a9e10be9c9b7a
onqtam <vik.kirilov@gmail.com>
2021-10-10 18:36:47 +0300

    sample data

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -20,13 +20,12 @@
           </v-list>
         </v-sheet>
       </v-col>
-
       <v-col>
-        <v-sheet
-          min-height="70vh"
-          rounded="lg"
-        >
-          <!--  -->
+        <v-sheet min-height="70vh" rounded="lg">
+          
+
+
+
         </v-sheet>
       </v-col>
     </v-row>
@@ -37,14 +36,53 @@
   export default {
     name: 'Main',
 
+    methods: [
+      // https://api.flipsidecrypto.com/api/v2/queries/     /data/latest
+    ],
+
     data: () => ({
       model: 0,
       dashboards: [
-        { name: 'network stats', icon: "mdi-lan" },
-        { name: 'supply', icon: "mdi-barcode" },
-        { name: 'user/address metrics', icon: "mdi-account-group" },
-        { name: 'liquidity, fees & income', icon: "mdi-oil" },
-        { name: 'miscellaneous', icon: "mdi-help-circle" },
+        {
+          name: 'network stats',
+          icon: "mdi-lan",
+          charts: [
+            { title: "m1", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+            { title: "m2", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+          ]
+        },
+        {
+          name: 'supply',
+          icon: "mdi-barcode",
+          charts: [
+            { title: "m1", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+            { title: "m2", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+          ]
+        },
+        {
+          name: 'user/address metrics',
+          icon: "mdi-account-group",
+          charts: [
+            { title: "m1", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+            { title: "m2", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+          ]
+        },
+        {
+          name: 'liquidity, fees & income',
+          icon: "mdi-oil",
+          charts: [
+            { title: "m1", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+            { title: "m2", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+          ]
+        },
+        {
+          name: 'miscellaneous',
+          icon: "mdi-help-circle",
+          charts: [
+            { title: "m1", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+            { title: "m2", source: "aab55e78-2c41-4708-b7a8-ed576556418a" },
+          ]
+        },
       ],
     }),
   }
